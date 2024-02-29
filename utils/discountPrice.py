4b28f45c97275826cb43d1fb9ee245930fb89a72
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2024-02-29 11:14:03 -0600

    float in refrendo

--- utils/discountPrice.py
+++ utils/discountPrice.py
@@ -1,7 +1,7 @@
 def discount_deprecation(price, discount):
-    parse_price_to_int = int(price)
+    parse_price_to_int = float(price)
     return parse_price_to_int * (1 - discount)
 
 def charge_appreciation(price, charge):
-    parse_price_to_int = int(price)
+    parse_price_to_int = float(price)
     return parse_price_to_int * (1 + charge)
